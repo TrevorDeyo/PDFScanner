90c53f9a3af8d2e22404589c6902b9cb386e3837
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-12-18 00:19:38 -0600

    Update pdf_text_extractor.py

--- pdf_text_extractor.py
+++ pdf_text_extractor.py
@@ -1,8 +1,15 @@
+import PyPDF2
 from PyPDF2 import PdfReader
+
+print(PyPDF2.__version__)
 
 # Load PDF
 reader = PdfReader("AQA GCSE textbook.pdf")
 
 # Extract text from each page
+# for page in reader.pages:
+i = 0
 for page in reader.pages:
-    print(page.extract_text())
+    if i < 2:
+        print(page.extract_text())
+        i += 1
